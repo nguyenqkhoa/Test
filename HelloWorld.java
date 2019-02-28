02192e7abf2156f706a44ad25d33bea404f4beb4
Khoa Nguyen <khoa.nguyen1@cs.ucalgary.ca>
2019-02-28 10:26:42 -0700

    Hello Worlds

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Worldsss");
+        System.out.println("Hello, Worlds");
     }
 
 }
